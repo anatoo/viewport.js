b3761f55d29a437e09dcf9b0e29fd891f1a65b83
anatoo <anatoo.jp@gmail.com>
2012-04-03 16:13:48 +0900

    Fixed ignored "width" prameter on iOS case.

--- viewport.js
+++ viewport.js
@@ -1,12 +1,23 @@
 /* 
-Copyright (c) 2012 Mitsunori Kubota<anatoo.jp@gmail.com>
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+ *  Copyright (c) 2012 Mitsunori Kubota<anatoo.jp@gmail.com>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy 
+ *  of this software and associated documentation files (the "Software"), to deal in 
+ *  the Software without restriction, including without limitation the rights to 
+ *  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies 
+ *  of the Software, and to permit persons to whom the Software is furnished to do 
+ *  so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all 
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS 
+ *  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+ *  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+ *  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION 
+ *  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
 
 (function() {
     var isDev = false;
@@ -21,10 +32,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
     };
 
     var defaultParams = {
-        width : 500,
-        userScalable : false,
-        minimumScale : 0.25,
-        maxScale : 2.0
+        width : 500
     };
 
     var merge = function(base, right) {
@@ -57,7 +65,7 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
         window.viewport = function(params) {
             d("iOS is detected");
             params = merge(defaultParams, params);
-            document.write('<meta name="viewport" content="width=500,user-scalable=false" />');
+            document.write('<meta name="viewport" content="width=' + params.width + ',user-scalable=false" />');
         };
     } else if (isAndroid()) {
         window.viewport = function(params) {
